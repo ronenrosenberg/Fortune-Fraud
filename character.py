1b24c1b3f47d06d4e69e11716fd635088c3a63cb
Knovnov <notknovus@gmail.com>
2024-03-12 15:44:44 -0700

    title

--- character.py
+++ character.py
@@ -1,6 +1,15 @@
 import pygame
+import json
+
+with open('dialogue.json') as f:
+    dialogue = json.load(f)
 
 #may be useful soon
 class Character():
-    def __init__(self):
+    def __init__(self, name):
+        self.name = name
+        dialogue_location = dialogue[self.name]
+        answers = []
+
+    def dialogue(self):
         pass
--- fortune_fraud.py
+++ fortune_fraud.py
@@ -19,7 +19,7 @@ pygame.display.set_caption("Fortune Fraud")
 #rectangle object that covers the entire screen (for testing purposes)
 screen_rect = screen.get_rect()
 #defines where the text box should be relative to the size of the screen (works-ish)
-text_center_x, text_center_y = int(screen_width * 0.5), int(screen_height * 0.7)
+text_center_x, text_center_y = int(screen_width * 0.4), int(screen_height * 0.8)
 text_width, text_height = int(screen_width * 0.5), int(screen_height * 0.2)
 text_rect = pygame.rect.Rect(text_center_x - (text_width//2), text_center_y - (text_height//2), text_width, text_height)
 
@@ -27,10 +27,11 @@ text_rect = pygame.rect.Rect(text_center_x - (text_width//2), text_center_y - (t
 clock = pygame.time.Clock()
 
 #load images
-background_image = pygame.image.load("assets/background.jpeg") 
-curtain_image = pygame.image.load("assets/curtain.png") 
-crystal_ball_image = pygame.image.load("assets/crystal_ball.png") 
-border_image = pygame.image.load("assets/border.png") 
+background_image = pygame.image.load("assets/background.PNG") 
+curtain_image = pygame.image.load("assets/curtain.PNG") 
+crystal_ball_image = pygame.image.load("assets/crystal_ball.PNG") 
+border_image = pygame.image.load("assets/border.PNG") 
+text_box_image = pygame.image.load("assets/text_box.PNG") 
 
 running = True
 while running:
@@ -52,9 +53,11 @@ while running:
     scale_to_fullscreen(screen, curtain_image)
     scale_to_fullscreen(screen, crystal_ball_image)
     scale_to_fullscreen(screen, border_image)
+    scale_to_fullscreen(screen, border_image)
+    scale_to_fullscreen(screen, text_box_image)
 
     test_text = "Ooh I am a mystical fortune teller and I want to tell you your very real fortuen hahahaksdhjfaklsdjfaljksjahsdlf asdhlf asdjf hasjkd faskdfalsjkdhfajshdfkahsdf"
-    textWrap(screen, test_text, "white", text_rect, pygame.font.Font("Vollkorn.ttf", 24))
+    textWrap(screen, test_text, "black", text_rect, pygame.font.Font("Vollkorn.ttf", 28))
    
 
     # flip() the display to put your work on screen
